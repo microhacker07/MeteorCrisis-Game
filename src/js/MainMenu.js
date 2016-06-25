0e02708be0cce589415ae732ba22014f37acf7af
microhacker07 <bock.nathaniel@gmail.com>
2016-06-25 18:34:55 -0400

    Meteor Crisis - 0.1.0
    This is the first version of Meteor Crisis
    -It is still under development

---
+++ src/js/MainMenu.js
@@ -0,0 +1,83 @@
+MeteorCrisis.StateMainMenu = function (game) {
+	
+	var menuBackground;
+	var logo
+	var button_play;
+	var text_play;
+	var button_help;
+	var text_help;
+};
+
+MeteorCrisis.StateMainMenu.prototype = {
+	
+	create: function() {
+	
+		menuBackground = this.add.sprite(0, 0, 'menuBackground');
+		
+		logo = this.add.sprite(this.world.centerX - 223, 75, 'logo');
+	
+		button_play = this.add.button(this.world.centerX - 80, 250, 'button', this.buttonPressedPlay, this, 1, 0, 2);
+		
+		button_help = this.add.button(this.world.centerX - 80, 355, 'button', this.buttonPressedHelp, this, 1, 0, 2);
+		
+		var buttonStyle = { font: "60px Tahoma", fill: "#000000", wordWrap: true, wordWrapWidth: button_play.width, align: "center"};
+		
+		text_play = this.add.text(0, 0, "Play", buttonStyle);
+		text_play.alignIn(button_play, Phaser.CENTER, 0, 0);
+	
+		text_help = this.add.text(0, 0, "Help", buttonStyle);
+		text_help.alignIn(button_help, Phaser.CENTER, 0, 0);
+	},
+	
+	buttonPressedPlay: function() {
+		
+		//Start the next state
+		this.state.start('Game');
+		
+	},
+	
+	buttonPressedHelp: function() {
+		
+		//Start the next state
+		this.state.start('HelpMenu');
+		
+	}
+};
+
+MeteorCrisis.StateHelpMenu = function (game) {
+	
+	var menuBackground;
+	var logo;
+	var text_help;
+	var button_back;
+	var text_back;
+};
+
+MeteorCrisis.StateHelpMenu.prototype = {
+	
+	create: function() {
+	
+		menuBackground = this.add.sprite(0, 0, 'menuBackground');
+		
+		logo = this.add.sprite(this.world.centerX - 223, 75, 'logo');
+		
+		button_back = this.add.button(this.world.centerX - 80, 495, 'button', this.buttonPressedBack, this, 1, 0, 2);
+		
+		var buttonStyle = { font: "60px Tahoma", fill: "#000000", wordWrap: true, wordWrapWidth: button_back.width, align: "center"};
+		
+		text_back = this.add.text(0, 0, "Back", buttonStyle);
+		text_back.alignIn(button_back, Phaser.CENTER, 0, 0);
+		
+		subText_help = this.add.text(this.world.centerX - 20, 175, "Help", {font: "30px Arial", fill: "#FFFFFF", align: "center"});
+		
+		text_help = this.add.text(175, 225, "Movement: W,A,S,D or Arrow Keys \nFire laser: Press Spacebar\n Goal: Try to shoot as many meteors as you can and do not explode!", {font: "26px Courier New", fill: "#FFFFFF", wordWrap: true, wordWrapWidth: 600, align: "center"});
+	
+	},
+	
+	buttonPressedBack: function() {
+		
+		//Start the next state
+		this.state.start('MainMenu');
+		
+	}
+};
---
+++ src/js/Preloader.js
@@ -0,0 +1,50 @@
+var MeteorCrisis = {};
+
+MeteorCrisis.StatePreloader = function (game) {
+	
+    this.text = null;
+
+};
+
+MeteorCrisis.StatePreloader.prototype = {
+	
+	init: function() {
+	
+		this.text = this.add.text(this.world.centerX, this.world.centerY, "Loading: 0%", { font: "32px Arial", fill: "#ffffff", align: "center" });
+		this.text.anchor.x = 0.5;
+		
+	},
+
+    preload: function() {
+
+		//Load all the images
+        this.load.image('background', 'assets/space.png');
+		this.load.image('menuBackground', 'assets/MainMenuBackground.png');
+		this.load.image('stars1', 'assets/stars1.png');
+		this.load.image('stars2', 'assets/stars2.png');
+        this.load.image('logo', 'assets/MeteorCrisis_logo.png');
+		this.load.spritesheet('button', 'assets/button.png', 160, 80);
+		this.load.image('meteor', 'assets/meteor.png');
+		this.load.image('laser', 'assets/laser.png');
+		this.load.spritesheet('explosion', 'assets/explosion.png', 50, 38);
+		this.load.spritesheet('spacecraft', 'assets/spacecraft.png', 82, 48);
+		
+		this.load.onFileComplete.add(this.fileLoaded, this);
+    },
+	
+	fileLoaded: function(progress) {
+
+        this.text.text = "Loading: " + progress + "%";
+
+    },
+
+    create: function() {
+
+		//Start the next state
+        this.state.start('MainMenu');
+		
+    }
+
+};
+
+
